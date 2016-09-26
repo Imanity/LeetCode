86b017ab8b0a44f00bc9353b01c2512cf9118c07
Imanity <imanity14@126.com>
2016-09-27 03:54:22 +0800

    Question 3 finished

---
+++ 3 Longest Substring/3 Longest Substring/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Solution {
+public:
+	int lengthOfLongestSubstring(string s) {
+		int maxLength = 0, currentLength = 0;
+		for (int i = 0; i < s.length(); ++i) {
+			char tmp = s[i];
+			currentLength++;
+			for (int j = i - 1; j >= i - currentLength && j >= 0; --j) {
+				if (s[j] == tmp) {
+					currentLength = i - j;
+					break;
+				}
+			}
+			if (currentLength > maxLength) {
+				maxLength = currentLength;
+			}
+		}
+		return maxLength;
+	}
+};
+
+int main() {
+	Solution sol;
+	cout << sol.lengthOfLongestSubstring("abcabcbb");
+	return 0;
+}
